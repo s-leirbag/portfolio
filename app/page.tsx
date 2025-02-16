014801b452a4d971170eda3d943382179c17d71a
Gabriel Shiu <gabriel@shiu.us>
2025-02-16 17:49:31 -0500

    work section in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,33 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <main className="w-full sm:w-[640] md:w-[768] lg:w-[1024] h-screen p-16 flex flex-col gap-8 items-center justify-center sm:items-start">
-        <div className="w-full flex flex-col md:flex-row gap-12">
-          <Image
-            className="rounded-2xl lg:w-64 lg:h-64 w-48 h-48 md:order-1"
-            src="/professional_photo.jpeg"
-            alt="Next.js logo"
-            width={2344}
-            height={2344}
-            priority
-          />
-          <div className="flex-1 flex flex-col gap-2">
-            <h1 className="text-5xl sm:text-8xl py-0.5 rounded font-semibold">
-              Gabriel Shiu
-            </h1>
-            <p>
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra
-              mus phasellus potenti hac. Class blandit gravida semper; pharetra
-              amet nibh sem dis? Donec consequat sagittis augue curabitur
-              dapibus ornare dapibus facilisis. Venenatis maecenas eros
-              suspendisse nec ultrices platea. Bibendum massa nibh ridiculus
-              lorem mus. Egestas
-            </p>
+    <div className="justify-items-center font-[family-name:var(--font-geist-sans)]">
+      <main className="w-full sm:w-[640] md:w-[768] lg:w-[1024]">
+        <div className="h-screen p-16 flex flex-col justify-center">
+          <div className="w-full flex flex-col md:flex-row gap-12">
+            <Image
+              className="rounded-2xl lg:w-64 lg:h-64 w-48 h-48 md:order-1"
+              src="/professional_photo.jpeg"
+              alt="portrait photo"
+              width={2344}
+              height={2344}
+              priority
+            />
+            <div className="flex-1 flex flex-col gap-2">
+              <h1 className="text-5xl sm:text-8xl py-0.5 rounded font-semibold">
+                Gabriel Shiu
+              </h1>
+              <p>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum
+                nostra mus phasellus potenti hac. Class blandit gravida semper;
+                pharetra amet nibh sem dis? Donec consequat sagittis augue
+                curabitur dapibus ornare dapibus facilisis. Venenatis maecenas
+                eros suspendisse nec ultrices platea. Bibendum massa nibh
+                ridiculus lorem mus. Egestas
+              </p>
+            </div>
           </div>
         </div>
         <a
@@ -35,6 +38,73 @@ export default function Home() {
         >
           My Work
         </a>
+        <div className="w-full py-16">
+          <div className="w-full md:w-[640] lg:w-[768] px-16 m-auto">
+            <h2 className="text-3xl sm:text-6xl py-0.5 rounded font-semibold">
+              Work
+            </h2>
+            <p>Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
+          </div>
+          <div className="mt-8 w-full px-16 grid grid-cols-2 gap-8">
+            <div className="bg-neutral-800 border-neutral-700 border rounded-xl p-6 flex flex-col gap-2">
+              <h3 className="text-xl sm:text-3xl rounded font-semibold">
+                Rubiks Cube Trainer
+              </h3>
+              <p>Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
+              <Link
+                href={"/work/oll-trainer"}
+                className="font-medium hover:underline"
+              >
+                view project {"->"}
+              </Link>
+              <Image
+                src="/train-mui.png"
+                alt="project thumbnail"
+                width={2852}
+                height={2112}
+                priority
+              />
+            </div>
+            <div className="bg-neutral-800 border-neutral-700 border rounded-xl p-6 flex flex-col gap-2">
+              <h3 className="text-xl sm:text-3xl rounded font-semibold">
+                Rubiks Cube Trainer
+              </h3>
+              <p>Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
+              <Link
+                href={"/work/oll-trainer"}
+                className="font-medium hover:underline"
+              >
+                view project {"->"}
+              </Link>
+              <Image
+                src="/train-mui.png"
+                alt="project thumbnail"
+                width={2852}
+                height={2112}
+                priority
+              />
+            </div>
+            <div className="bg-neutral-800 border-neutral-700 border rounded-xl p-6 flex flex-col gap-2">
+              <h3 className="text-xl sm:text-3xl rounded font-semibold">
+                Rubiks Cube Trainer
+              </h3>
+              <p>Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
+              <Link
+                href={"/work/oll-trainer"}
+                className="font-medium hover:underline"
+              >
+                view project {"->"}
+              </Link>
+              <Image
+                src="/train-mui.png"
+                alt="project thumbnail"
+                width={2852}
+                height={2112}
+                priority
+              />
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
