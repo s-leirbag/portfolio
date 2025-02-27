66e5ceedb932876e88f59e5c65f074f5b378b3ae
Gabriel Shiu <gabriel@shiu.us>
2025-02-27 12:44:13 -0500

    rest of oll trainer project page

--- app/work/oll-trainer/page.tsx
+++ app/work/oll-trainer/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div className="justify-items-center font-[family-name:var(--font-geist-sans)]">
       <main className="w-full sm:w-[640] md:w-[768] lg:w-[1024]">
-        <div className="w-full pt-[calc(30vh)] p-16 gap-8 flex flex-col justify-center">
+        <div className="w-full pt-[calc(30vh)] p-16 gap-8 flex flex-col">
           <h1 className="text-5xl sm:text-8xl py-0.5 rounded font-semibold">
             {"Rubik's"} Cube Trainer
           </h1>
@@ -40,11 +40,136 @@ export default function Page() {
             </div>
           </div>
         </div>
-        <div className="w-full h-screen p-16 flex flex-col justify-center">
+        <div className="w-full p-16 gap-32 flex flex-col">
+          <Image
+            src="/train-mui.png"
+            alt="project thumbnail"
+            width={2852}
+            height={2112}
+            priority
+          />
+          <div>
+            <h3 className="text-2xl sm:text-5xl mb-2 rounded font-semibold">
+              Project Purpose
+            </h3>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra
+              mus phasellus potenti hac. Class blandit gravida semper; pharetra
+              amet nibh sem dis?
+            </p>
+          </div>
+          <div className="text-center lg:px-32">
+            <h3 className="text-2xl sm:text-5xl mb-2 rounded font-semibold">
+              Thought Process
+            </h3>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra
+              mus phasellus potenti hac. Class blandit gravida semper; pharetra
+              amet nibh sem dis?
+            </p>
+          </div>
+          <div className="lg:grid lg:grid-cols-2 gap-8">
+            <Image
+              src="/train-mui.png"
+              alt="project thumbnail"
+              width={2852}
+              height={2112}
+              priority
+            />
+            <div>
+              <h3 className="text-2xl sm:text-5xl mb-2 rounded font-semibold">
+                Problem
+              </h3>
+              <p>
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum
+                nostra mus phasellus potenti hac. Class blandit gravida semper;
+                pharetra amet nibh sem dis?
+              </p>
+            </div>
+          </div>
+          <div className="lg:pr-64">
+            <h3 className="text-2xl sm:text-5xl mb-2 rounded font-semibold">
+              Learnings
+            </h3>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra
+              mus phasellus potenti hac. Class blandit gravida semper; pharetra
+              amet nibh sem dis?
+            </p>
+          </div>
+        </div>
+        <div className="w-full pt-32">
+          <div className="w-full md:w-[640] lg:w-[768] px-16 m-auto">
+            <h2 className="text-3xl sm:text-6xl py-0.5 rounded font-semibold">
+              Other Work
+            </h2>
+            <p>Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
+          </div>
+          <div className="mt-8 w-full px-16 grid grid-cols-2 gap-8">
+            <div className="bg-neutral-800 border-neutral-700 border rounded-xl p-6 flex flex-col gap-2">
+              <h3 className="text-xl sm:text-3xl rounded font-semibold">
+                Rubiks Cube Trainer
+              </h3>
+              <p>Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
+              <Link
+                href={"/work/oll-trainer"}
+                className="font-medium hover:underline"
+              >
+                view project {"->"}
+              </Link>
+              <Image
+                src="/train-mui.png"
+                alt="project thumbnail"
+                width={2852}
+                height={2112}
+                priority
+              />
+            </div>
+            <div className="bg-neutral-800 border-neutral-700 border rounded-xl p-6 flex flex-col gap-2">
+              <h3 className="text-xl sm:text-3xl rounded font-semibold">
+                Rubiks Cube Trainer
+              </h3>
+              <p>Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
+              <Link
+                href={"/work/oll-trainer"}
+                className="font-medium hover:underline"
+              >
+                view project {"->"}
+              </Link>
+              <Image
+                src="/train-mui.png"
+                alt="project thumbnail"
+                width={2852}
+                height={2112}
+                priority
+              />
+            </div>
+            <div className="bg-neutral-800 border-neutral-700 border rounded-xl p-6 flex flex-col gap-2">
+              <h3 className="text-xl sm:text-3xl rounded font-semibold">
+                Rubiks Cube Trainer
+              </h3>
+              <p>Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
+              <Link
+                href={"/work/oll-trainer"}
+                className="font-medium hover:underline"
+              >
+                view project {"->"}
+              </Link>
+              <Image
+                src="/train-mui.png"
+                alt="project thumbnail"
+                width={2852}
+                height={2112}
+                priority
+              />
+            </div>
+          </div>
+        </div>
+        <div className="w-full h-[calc(90vh)] p-16 flex flex-col justify-center">
           <div className="flex flex-col gap-2">
-            <h1 className="text-3xl sm:text-6xl py-0.5 rounded font-semibold">
+            <h2 className="text-3xl sm:text-6xl py-0.5 rounded font-semibold">
               Contact Me
-            </h1>
+            </h2>
             <p>
               Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra
               mus phasellus potenti hac. Class blandit gravida semper; pharetra
