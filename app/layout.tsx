594978275f6b928cb0bfd497b43f26d5e71678bf
Gabriel Shiu <gabriel@shiu.us>
2025-03-14 01:50:14 -0400

    added logo to return home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -19,20 +21,20 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/logo-1-dot-fat-light.png',
+        url: "/logo-1-dot-fat-light.png",
       },
       {
-        media: '(prefers-color-scheme: dark)',
-        url: '/logo-1-dot-fat-dark.png',
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-1-dot-fat-dark.png",
       },
     ],
     apple: [
       {
-        url: '/logo-1-dot-fat-180-light.png',
+        url: "/logo-1-dot-fat-180-light.png",
       },
       {
-        media: '(prefers-color-scheme: dark)',
-        url: '/logo-1-dot-fat-180-light.png',
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-1-dot-fat-180-light.png",
       },
     ],
   },
@@ -48,6 +50,22 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-neutral-50`}
       >
+        <div
+          className="absolute w-32 p-8"
+          style={{
+            background: "radial-gradient(ellipse, black 0%, transparent 70%)",
+          }}
+        >
+          <Link href={"/"}>
+            <Image
+              src="/logo-1-dot-fat-dark.png"
+              alt="logo"
+              width={512}
+              height={512}
+              priority
+            />
+          </Link>
+        </div>
         {children}
         <Analytics />
       </body>
