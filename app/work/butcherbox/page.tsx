d4efd4aa6478224a57bd14cc2dbf265608cac9a5
Gabriel Shiu <gabriel@shiu.us>
2025-03-29 16:57:52 -0400

    bb page draft

--- app/work/butcherbox/page.tsx
+++ app/work/butcherbox/page.tsx
@@ -9,34 +9,47 @@ export default function Page() {
           <h1 className="text-5xl md:text-8xl py-0.5 rounded font-semibold bg-gradient-to-r from-sky-500 to-sky-300 dark:to-sky-200 bg-clip-text text-transparent">
             ButcherBox Internship
           </h1>
-          <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra
-            mus phasellus potenti hac. Class blandit gravida semper; pharetra
-            amet nibh sem dis? Donec consequat sagittis augue curabitur dapibus
-            ornare dapibus facilisis. Venenatis maecenas eros suspendisse nec
-            ultrices platea. Bibendum massa nibh ridiculus lorem mus. Egestas
-          </p>
-          <div className="pt-4 grid grid-cols-[repeat(auto-fit,minmax(120px,1fr))] gap-2 gap-y-8 max-w-lg">
-            <div className="space-y-2">
-              <strong>Stack</strong>
-              <p>React</p>
-              <p>Contentful</p>
-              <p>Datadog</p>
-            </div>
-            <div className="space-y-2">
-              <strong>Year</strong>
-              <p>2024</p>
-            </div>
-            <div className="space-y-2">
-              <strong>Link</strong>
+          <div className="flex flex-col gap-8 leading-loose text-neutral-600 dark:text-neutral-300">
+            <p>
+              Last summer, I interned at{" "}
               <Link
                 href={"https://www.butcherbox.com/"}
-                className="block w-fit rounded-lg bg-sky-500 hover:bg-[#0ea5e9e6] text-white py-4 px-4 sm:px-5"
+                className="hover:underline text-sky-500 hover:text-[#0ea5e9e6]"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                view site {"->"}
+                ButcherBox
               </Link>
+              , a subscription-based meat delivery service that specializes in
+              providing high-quality meat directly to customers&apos; doors! As
+              a Web Development Intern, I worked alongside one of their hybrid
+              teams, pair-programming and participating in standups/retros/etc.
+              This was my first internship. I got to learn so much!
+            </p>
+            <div>
+              I did 3 main things:
+              <ul className="list-disc list-inside pl-4">
+                <li>Develop a content-authoring tool</li>
+                <li>
+                  Increase Datadog observability by adding RUM actions and
+                  dashboards
+                </li>
+                <li>Refine UI components—and have fun issues with prefetching and responsiveness</li>
+              </ul>
+            </div>
+          </div>
+          <div className="pt-4 grid grid-cols-[repeat(auto-fit,minmax(120px,1fr))] gap-2 gap-y-8 max-w-lg">
+            <div className="space-y-2">
+              <strong>Stack</strong>
+              <p className="text-neutral-600 dark:text-neutral-300">React</p>
+              <p className="text-neutral-600 dark:text-neutral-300">
+                Contentful
+              </p>
+              <p className="text-neutral-600 dark:text-neutral-300">Datadog</p>
+            </div>
+            <div className="space-y-2">
+              <strong>Year</strong>
+              <p className="text-neutral-600 dark:text-neutral-300">2024</p>
             </div>
           </div>
         </div>
@@ -51,51 +64,128 @@ export default function Page() {
         />
         <div>
           <h3 className="text-2xl md:text-5xl mb-2 rounded font-semibold">
-            Project Purpose
+            Main Project
           </h3>
-          <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra
-            mus phasellus potenti hac. Class blandit gravida semper; pharetra
-            amet nibh sem dis?
-          </p>
-        </div>
-        <div className="text-center lg:px-32">
-          <h3 className="text-2xl md:text-5xl mb-2 rounded font-semibold">
-            Thought Process
-          </h3>
-          <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra
-            mus phasellus potenti hac. Class blandit gravida semper; pharetra
-            amet nibh sem dis?
+          <p className="leading-loose text-neutral-600 dark:text-neutral-300">
+            My main project had to do with our Content Management System,
+            Contentful! We had a bunch of flexible but complex content templates
+            that pained content authors each week. These templates allowed for a
+            variety configurations of headings, positionings, colors, and links.
+            This gave great customization and standardization across our site!
+            But, since Contentful is headless, by default we can&apos;t
+            visualize what configurations are possible within our templates. We
+            had to reference textual documentation in a tedious manner, wasting
+            time and risking making mistakes or creating worse content designs.
           </p>
         </div>
         <div className="lg:grid lg:grid-cols-2 gap-8">
           <Image
-            src="/butcherbox-ss.png"
-            alt="project thumbnail"
-            width={3248}
-            height={2112}
-            priority
+            src="/visualize.svg"
+            alt="Visualizing Content Templates"
+            width={1920}
+            height={1080}
+            className="dark:hidden m-auto"
+          />
+          <Image
+            src="/visualize-dark.svg"
+            alt="Visualizing Content Templates"
+            width={1920}
+            height={1080}
+            className="hidden dark:inline-block m-auto"
           />
           <div>
             <h3 className="text-2xl md:text-5xl mb-2 rounded font-semibold">
-              Problem
+              Visualizing Content Templates
             </h3>
-            <p>
-              Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra
-              mus phasellus potenti hac. Class blandit gravida semper; pharetra
-              amet nibh sem dis?
+            <p className="leading-loose text-neutral-600 dark:text-neutral-300">
+              Our solution was to create a visual web tool. Authors would use it
+              to view all possible content configurations, filter for certain
+              characteristics, and easily see what configurations are possible
+              within our templates.
             </p>
           </div>
+        </div>
+        <div className="text-center lg:px-32">
+          <h3 className="text-2xl md:text-5xl mb-2 rounded font-semibold">
+            Development Process
+          </h3>
+          <p className="leading-loose text-neutral-600 dark:text-neutral-300">
+            We built this tool into an existing internal web app, extending its
+            features. I referenced Figma mockups to create new routes and lay
+            out the pages. One huge part of the project was generating all
+            possible content permutations from our templates. I paired with an
+            engineer and used test-driven development to cover all possible
+            content permutations and detailed nuances. This required me to gain
+            a thorough understanding of our content model and spend a ton of
+            time in Contentful.
+          </p>
+        </div>
+        <div className="lg:grid lg:grid-cols-2 gap-8">
+          <Image
+            src="/filter.svg"
+            alt="filtering"
+            width={1920}
+            height={1080}
+            className="dark:hidden m-auto"
+          />
+          <Image
+            src="/filter-dark.svg"
+            alt="filtering"
+            width={1920}
+            height={1080}
+            className="hidden dark:inline-block m-auto"
+          />
+          <div>
+            <h3 className="text-2xl md:text-5xl mb-2 rounded font-semibold">
+              Challenge: Filtering
+            </h3>
+            <p className="leading-loose text-neutral-600 dark:text-neutral-300">
+              A key feature was letting content authors filter for possible
+              configurations, like having a background image and one heading. I
+              found that such a simple feature like filtering can actually be
+              hard to implement! You have to code the UI components, their
+              state, and then the actual filtering code. You have to create
+              clean, modular data structures to represent filter options, UI
+              state, and the filters to apply. This was a surprising challenge
+              but I really enjoyed working through it! I now have a greater
+              appreciation for filters.
+            </p>
+          </div>
+        </div>
+        <div className="text-center lg:px-32">
+          <h3 className="text-2xl md:text-5xl mb-2 rounded font-semibold">
+            Impact
+          </h3>
+          <p className="leading-loose text-neutral-600 dark:text-neutral-300">
+            While developing this tool, we demo&apos;d our progress in sprint
+            reviews and the tool began to be used even during development!
+            Overall, it helped content authors more easily create great
+            copywriting to help customers get high-quality meat to their
+            stomachs!
+          </p>
         </div>
         <div className="lg:pr-64">
           <h3 className="text-2xl md:text-5xl mb-2 rounded font-semibold">
             Learnings
           </h3>
-          <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra
-            mus phasellus potenti hac. Class blandit gravida semper; pharetra
-            amet nibh sem dis?
+          <p className="leading-loose text-neutral-600 dark:text-neutral-300">
+            I learned a ton about Contentful, Datadog, React Router, Storybook,
+            and Jest. I got to dive deeper into the basics of Typescript, React,
+            Git, and even Chrome Dev Tools for debugging.
+            <br />
+            <br />
+            I was surprised how little time you spend actually writing code
+            compared to reading code; having meetings; and writing tickets,
+            PR&apos;s, and documentation. I loved practicing technical
+            communication, planning for meetings, and presenting my work. I
+            learned to break down PR&apos;s to make things easy for my team to
+            review.
+            <br />
+            <br />I also learned a lot from pair-programming. I learned to write
+            pseudocode, discern what rabbit holes to pursue or leave, and the
+            usefulness of test-driven development. I loved working in a real
+            engineering environment. I love growing my hard and soft skills, and
+            have a long way to go!
           </p>
         </div>
       </div>
@@ -104,8 +194,9 @@ export default function Page() {
           <h2 className="text-3xl md:text-6xl py-0.5 rounded font-semibold text-shadow-50px">
             Other Work
           </h2>
-          <p className="text-shadow-20px">
-            Lorem ipsum odor amet, consectetuer adipiscing elit.
+          <p className="text-shadow-20px leading-loose text-neutral-600 dark:text-neutral-300">
+            I learn by doing! Take a look at some of my past and ongoing
+            projects.
           </p>
         </div>
         <div className="w-full sm:w-[640px] md:w-[768px] lg:w-[1024px] pt-8 pb-16 px-8 md:p-16 mx-auto grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-8">
@@ -116,15 +207,16 @@ export default function Page() {
             <h3 className="text-xl md:text-3xl font-semibold">
               Citizens Bank Internship
             </h3>
-            <p>Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
-            <p className="font-medium hover:underline">view project {"->"}</p>
+            <p className="text-neutral-600 dark:text-neutral-300">
+              My time as an intern at Citizens Bank.
+            </p>
+            <p className="font-medium hover:underline">read story {"->"}</p>
             <div className="flex-1 flex">
               <Image
                 src="/citizens-ss.jpeg"
                 alt="project thumbnail"
                 width={4096}
                 height={1828}
-                priority
                 className="m-auto"
               />
             </div>
@@ -136,7 +228,9 @@ export default function Page() {
             <h3 className="text-xl md:text-3xl font-semibold">
               Rubiks Cube Trainer
             </h3>
-            <p>Lorem ipsum odor amet, consectetuer adipiscing elit.</p>
+            <p className="text-neutral-600 dark:text-neutral-300">
+              My first web project!
+            </p>
             <p className="font-medium hover:underline">view project {"->"}</p>
             <div className="flex-1 flex">
               <Image
@@ -144,7 +238,6 @@ export default function Page() {
                 alt="project thumbnail"
                 width={3248}
                 height={2112}
-                priority
               />
             </div>
           </Link>
@@ -155,12 +248,8 @@ export default function Page() {
           <h2 className="text-3xl md:text-6xl py-0.5 rounded font-semibold">
             Contact Me
           </h2>
-          <p>
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra
-            mus phasellus potenti hac. Class blandit gravida semper; pharetra
-            amet nibh sem dis? Donec consequat sagittis augue curabitur dapibus
-            ornare dapibus facilisis. Venenatis maecenas eros suspendisse nec
-            ultrices platea. Bibendum massa nibh ridiculus lorem mus. Egestas
+          <p className="leading-loose text-neutral-600 dark:text-neutral-300">
+            You can find me on social media! Email form coming soon...
           </p>
           <div className="flex flex-row">
             <a
