a9f20da58ade6cddd62e4dfaacbe349863e2260d
Gabriel Shiu <gabriel@shiu.us>
2025-04-10 12:28:32 -0400

    oll trainer migrated

---
+++ app/data/oll-trainer.ts
@@ -0,0 +1,106 @@
+import { ProjectContent } from "../lib/content";
+
+const ollTrainerProject: ProjectContent = {
+  title: "Rubik's Cube Trainer",
+  blurb: "A training application for Rubik's Cube algorithms",
+  href: "/rubiks-cube",
+  cta: "view project",
+  image: {
+    src: "/oll-trainer/oll-trainer-ss.png",
+    alt: "Rubik's Cube Trainer screenshot",
+    width: 3248,
+    height: 2112
+  },
+  description: [
+    {
+      type: "paragraph",
+      children: [
+        { type: "text", value: "work in progress...enjoy some lorum ipsum" }
+      ]
+    }
+  ],
+  stack: ["React", "Express", "Material UI"],
+  year: 2022,
+  link: "https://oll-trainer-s-leirbag.vercel.app/",
+  linkCTA: "view site",
+  content: [
+    {
+      type: "image",
+      imageSet: {
+        src: "/oll-trainer/oll-trainer-ss.png",
+        alt: "project thumbnail",
+        width: 3248,
+        height: 2112
+      }
+    },
+    {
+      type: "textLeft",
+      heading: "Project Purpose",
+      content: [
+        {
+          type: "paragraph",
+          children: [
+            { 
+              type: "text", 
+              value: "Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra mus phasellus potenti hac. Class blandit gravida semper; pharetra amet nibh sem dis?" 
+            }
+          ]
+        }
+      ]
+    },
+    {
+      type: "textCenter",
+      heading: "Thought Process",
+      content: [
+        {
+          type: "paragraph",
+          children: [
+            { 
+              type: "text", 
+              value: "Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra mus phasellus potenti hac. Class blandit gravida semper; pharetra amet nibh sem dis?" 
+            }
+          ]
+        }
+      ]
+    },
+    {
+      type: "split",
+      imageRight: false,
+      imageSet: {
+        src: "/oll-trainer/oll-trainer-ss.png",
+        alt: "project thumbnail",
+        width: 3248,
+        height: 2112
+      },
+      heading: "Problem",
+      content: [
+        {
+          type: "paragraph",
+          children: [
+            { 
+              type: "text", 
+              value: "Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra mus phasellus potenti hac. Class blandit gravida semper; pharetra amet nibh sem dis?" 
+            }
+          ]
+        }
+      ]
+    },
+    {
+      type: "textRightPadding",
+      heading: "Learnings",
+      content: [
+        {
+          type: "paragraph",
+          children: [
+            { 
+              type: "text", 
+              value: "Lorem ipsum odor amet, consectetuer adipiscing elit. Rutrum nostra mus phasellus potenti hac. Class blandit gravida semper; pharetra amet nibh sem dis?" 
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+
+export default ollTrainerProject;
--- app/data/projects.ts
+++ app/data/projects.ts
@@ -1,9 +1,11 @@
 import butcherboxProject from "./butcherbox";
 import citizensProject from "./citizens";
 import hospitalProject from "./hospital";
+import ollTrainerProject from "./oll-trainer";
 
 export const projects = {
   butcherbox: butcherboxProject,
   citizens: citizensProject,
   hospital: hospitalProject,
+  ollTrainer: ollTrainerProject,
 };
