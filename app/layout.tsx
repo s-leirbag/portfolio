b5f28d80d8a20710babe4923c330e5d1aa0d5fd1
Gabriel Shiu <gabriel@shiu.us>
2025-04-10 15:19:38 -0400

    added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Image from "next/image";
@@ -71,6 +72,7 @@ export default function RootLayout({
         </div>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
